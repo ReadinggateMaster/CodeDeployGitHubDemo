dbb40a2411a3182ff0eede7baca107bf239f6abb
김용정 <rlfvhtm@gmail.com>
2023-02-14 12:22:20 +0900

    change sh file

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/npm /usr/bin/npm
+sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/pm2 /usr/bin/pm2
+
 cd /home/ec2-user/workspace/readinggate-rest-api-nest-server
 
 sudo npm install
