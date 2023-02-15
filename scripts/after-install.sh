ee2b636084e9470f4488e75330679bd58042bbd6
김용정 <rlfvhtm@gmail.com>
2023-02-15 09:36:06 +0900

    change after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo ln -s which node /usr/local/bin/node
-sudo ln -s which npm /usr/local/bin/npm
-sudo ln -s which pm2 /usr/local/bin/pm2
+sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/node /usr/local/bin/node
+sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/npm /usr/local/bin/npm
+sudo ln -s which /root/.nvm/versions/node/v16.19.0/bin/pm2 /usr/local/bin/pm2
 
 sudo npm -v
 
