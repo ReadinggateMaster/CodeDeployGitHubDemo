cc7bf8f92e4bf770dea531b8b07008ada4a01cda
rlfvhtm <rlfvhtm@gmail.com>
2023-02-17 09:31:36 +0900

    change sh file

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/node /usr/local/bin
-sudo ln -s /root/.nvm/versions/node/v16.19.0/bin/npm /usr/local/bin
-sudo ln -s which /root/.nvm/versions/node/v16.19.0/bin/pm2 /usr/local/bin
-
 sudo npm -v
 
 cd /home/ec2-user/workspace/readinggate-rest-api-nest-server
