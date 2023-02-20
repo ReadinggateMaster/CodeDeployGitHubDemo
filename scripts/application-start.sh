622063403b0876306a967fa8e6408ec4def4031e
rlfvhtm <rlfvhtm@gmail.com>
2023-02-21 08:10:38 +0900

    change sh files

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,6 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-sudo pm2 ls
+cd /home/ec2-user/workspace/readinggate-rest-api-nest-server
 
 sudo pm2 delete main
+
+sudo pm2 start dist/main.js
